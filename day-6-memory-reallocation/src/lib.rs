7f87e019362557b4621227eb9d24579d4ba36700
Thomas van der Burgt <thomas@thvdburgt.nl>
2017-12-06 16:51:22 +0100

    solved day 6

--- day-6-memory-reallocation/src/lib.rs
+++ day-6-memory-reallocation/src/lib.rs
@@ -1,7 +1,60 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 pub fn solve_puzzle_part_1(input: &str) -> String {
-    ":(".to_string()
+    let mut banks = string_to_nums(input);
+
+    let mut seen = HashSet::new();
+    let mut cycle = 0;
+    while !seen.contains(&banks) {
+        seen.insert(banks.clone());
+        cycle_bank(&mut banks);
+        cycle += 1;
+    }
+
+    cycle.to_string()
 }
 
 pub fn solve_puzzle_part_2(input: &str) -> String {
-    ":(".to_string()
+    let mut banks = string_to_nums(input);
+
+    let mut seen = HashMap::new();
+    let mut cycle: u32 = 0;
+    while !seen.contains_key(&banks) {
+        seen.insert(banks.clone(), cycle);
+        cycle_bank(&mut banks);
+        cycle += 1;
+    }
+
+    (cycle - seen[&banks]).to_string()
+}
+
+fn string_to_nums(line: &str) -> Vec<u32> {
+    line.split_whitespace()
+        .map(|string| string.parse::<u32>().unwrap())
+        .collect()
+}
+
+
+fn cycle_bank(banks: &mut Vec<u32>) {
+    // find the memory bank with the most blocks (use of negative index to make sure ties are won
+    // by the lowest-numbered memory bank)
+    let (max_i, &max_val) = banks
+        .iter()
+        .enumerate()
+        .max_by_key(|&(i, x)| (x, -(i as isize)))
+        .expect("empty list of banks");
+    let banks_len = banks.len();
+
+    banks[max_i] = 0;
+    // add quotient to each bank
+    let quotient = max_val / banks_len as u32;
+    for x in banks.iter_mut() {
+        *x += quotient;
+    }
+    // add 1 to ecah bank for 'remainder' banks from max bank
+    let remainder = max_val % banks_len as u32;
+    for i in (max_i + 1)..(max_i + 1 + remainder as usize) {
+        banks[i % banks_len] += 1;
+    }
 }
--- day-6-memory-reallocation/tests/tests.rs
+++ day-6-memory-reallocation/tests/tests.rs
@@ -3,6 +3,11 @@ extern crate advent_of_code_2017_day_6;
 use advent_of_code_2017_day_6::*;
 
 #[test]
-fn part_1_example_1() {
+fn part_1_example() {
     assert_eq!(solve_puzzle_part_1("0 2 7 0"), "5");
+}
+
+#[test]
+fn part_2_example() {
+    assert_eq!(solve_puzzle_part_2("0 2 7 0"), "4");
 }
