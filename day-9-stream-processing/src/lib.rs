eb3c1b1b05584605028c559016f3fbe8c0c159a5
Thomas van der Burgt <thomas@thvdburgt.nl>
2017-12-09 17:36:25 +0100

    solved day 9

--- day-9-stream-processing/src/lib.rs
+++ day-9-stream-processing/src/lib.rs
@@ -1,7 +1,35 @@
 pub fn solve_puzzle_part_1(input: &str) -> String {
-    ":(".to_string()
+    solve(input).0.to_string()
 }
 
 pub fn solve_puzzle_part_2(input: &str) -> String {
-    ":(".to_string()
+    solve(input).1.to_string()
+}
+
+fn solve(input: &str) -> (u32, u32) {
+    let mut in_garbage = false;
+    let mut level = 0;
+    let mut score = 0;
+    let mut garbage_count = 0;
+
+    let mut chars = input.chars();
+    while let Some(c) = chars.next() {
+        match c {
+            '!' => {
+                chars.next();
+            }
+            '>' => in_garbage = false,
+            _ if in_garbage => garbage_count += 1,
+            '<' => in_garbage = true,
+            '{' => level += 1,
+            '}' => {
+                score += level;
+                level -= 1
+            }
+            ',' => {}
+            _ => panic!("found {}", c),
+        };
+    }
+
+    (score, garbage_count)
 }
--- day-9-stream-processing/tests/tests.rs
+++ day-9-stream-processing/tests/tests.rs
@@ -4,5 +4,38 @@ use advent_of_code_2017_day_9::*;
 
 #[test]
 fn part_1_example() {
-    // FIXME
+    // {}, score of 1.
+    assert_eq!(solve_puzzle_part_1("{}"), "1");
+    // {{{}}}, score of 1 + 2 + 3 = 6.
+    assert_eq!(solve_puzzle_part_1("{{{}}}"), "6");
+    // {{},{}}, score of 1 + 2 + 2 = 5.
+    assert_eq!(solve_puzzle_part_1("{{},{}}"), "5");
+    // {{{},{},{{}}}}, score of 1 + 2 + 3 + 3 + 3 + 4 = 16.
+    assert_eq!(solve_puzzle_part_1("{{{},{},{{}}}}"), "16");
+    // {<a>,<a>,<a>,<a>}, score of 1.
+    assert_eq!(solve_puzzle_part_1("{<a>,<a>,<a>,<a>}"), "1");
+    // {{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
+    assert_eq!(solve_puzzle_part_1("{{<ab>},{<ab>},{<ab>},{<ab>}}"), "9");
+    // {{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9."
+    assert_eq!(solve_puzzle_part_1("{{<!!>},{<!!>},{<!!>},{<!!>}}"), "9");
+    // {{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
+    assert_eq!(solve_puzzle_part_1("{{<a!>},{<a!>},{<a!>},{<ab>}}"), "3");
+}
+
+#[test]
+fn part_2_example() {
+    // <>, 0 characters.
+    assert_eq!(solve_puzzle_part_2("<>"), "0");
+    // <random characters>, 17 characters.
+    assert_eq!(solve_puzzle_part_2("<random characters>"), "17");
+    // <<<<>, 3 characters.
+    assert_eq!(solve_puzzle_part_2("<<<<>"), "3");
+    // <{!>}>, 2 characters.
+    assert_eq!(solve_puzzle_part_2("<{!>}>"), "2");
+    // <!!>, 0 characters.
+    assert_eq!(solve_puzzle_part_2("<!!>"), "0");
+    // <!!!>>, 0 characters.
+    assert_eq!(solve_puzzle_part_2("<!!!>>"), "0");
+    // <{o"i!a,<{i<a>, 10 characters.
+    assert_eq!(solve_puzzle_part_2("<{o\"i!a,<{i<a>"), "10");
 }
