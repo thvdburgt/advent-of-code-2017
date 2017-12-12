dcc2c8267bf8e7886a944a5707698bae78584b62
Thomas van der Burgt <thomas@thvdburgt.nl>
2017-12-12 16:15:11 +0100

    solved day 12

--- day-12-digital-plumber/src/lib.rs
+++ day-12-digital-plumber/src/lib.rs
@@ -1,7 +1,66 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn solve_puzzle_part_1(input: &str) -> String {
-    ":(".to_string()
+    let programs: HashMap<usize, HashSet<usize>> = input.lines().map(to_program).collect();
+
+    depth_first_search(&programs, 0).len().to_string()
 }
 
 pub fn solve_puzzle_part_2(input: &str) -> String {
-    ":(".to_string()
+    let programs: HashMap<usize, HashSet<usize>> = input.lines().map(to_program).collect();
+    let groups = program_groups(&programs);
+
+    groups.len().to_string()
+}
+
+fn to_program(s: &str) -> (usize, HashSet<usize>) {
+    let mut words = s.split_whitespace();
+    let program = words.next().unwrap().parse::<usize>().unwrap();
+    words.next();
+    let pipes = words
+        .map(|word| word.trim_right_matches(',').parse::<usize>().unwrap())
+        .collect();
+
+    (program, pipes)
+}
+
+fn program_groups(programs: &HashMap<usize, HashSet<usize>>) -> Vec<HashSet<usize>> {
+    let mut groups: Vec<HashSet<usize>> = Vec::new();
+
+    for node in programs.keys() {
+        let mut not_seen_before = true;
+        for group in &groups {
+            if group.contains(node) {
+                not_seen_before = false;
+                break;
+            }
+        }
+        if not_seen_before {
+            let group = depth_first_search(programs, *node);
+            groups.push(group);
+        }
+    }
+
+    groups
+}
+
+fn depth_first_search(programs: &HashMap<usize, HashSet<usize>>, node: usize) -> HashSet<usize> {
+    fn dfs_rec(
+        programs: &HashMap<usize, HashSet<usize>>,
+        node: usize,
+        visited: &mut HashSet<usize>,
+    ) {
+        visited.insert(node);
+
+        for neighbour in programs.get(&node).unwrap() {
+            if !visited.contains(neighbour) {
+                dfs_rec(programs, *neighbour, visited);
+            }
+        }
+    }
+
+    let mut visited: HashSet<usize> = HashSet::new();
+    dfs_rec(programs, node, &mut visited);
+
+    visited
 }
--- day-12-digital-plumber/tests/tests.rs
+++ day-12-digital-plumber/tests/tests.rs
@@ -4,5 +4,26 @@ use advent_of_code_2017_day_12::*;
 
 #[test]
 fn part_1_example() {
-    // FIXME
+    let input = "\
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+    assert_eq!(solve_puzzle_part_1(input), "6");
+}
+
+#[test]
+fn part_2_example() {
+    let input = "\
+0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5";
+    assert_eq!(solve_puzzle_part_2(input), "2");
 }
