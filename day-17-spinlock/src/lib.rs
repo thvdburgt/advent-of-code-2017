78c00a584f1c69fe12b093782c1d7afd01d5228f
Thomas van der Burgt <thomas@thvdburgt.nl>
2017-12-17 12:59:14 +0100

    make day 17 a bit uniformer

--- day-17-spinlock/src/lib.rs
+++ day-17-spinlock/src/lib.rs
@@ -3,7 +3,7 @@ pub fn solve_puzzle_part_1(steps: usize) -> u32 {
     let mut position = 0;
 
     for i in 0..2017 {
-        position = 1 + (position + steps) % buffer.len();
+        position = 1 + (position + steps) % (i as usize + 1);
         buffer.insert(position, i + 1);
     }
 
@@ -15,11 +15,11 @@ pub fn solve_puzzle_part_2(steps: usize) -> u32 {
 
     let mut val_after_zero = 0;
     for i in 0..50_000_000 {
-        position = 1 + (position + steps) % (i + 1);
+        position = 1 + (position + steps) % (i as usize + 1);
         if position == 1 {
             val_after_zero = i + 1
         }
     }
 
-    val_after_zero as u32
+    val_after_zero
 }
